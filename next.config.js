6d9f6cf0b0c4baee8adfbea867a19f413235ba9e
matsuo1972 <matsuo.freelance@gmail.com>
2025-04-09 11:47:27 +0900

    ビルドエラー解消

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
   experimental: {
     serverActions: {
@@ -17,4 +16,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.export = nextConfig;
